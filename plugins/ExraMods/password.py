9425eef193ddcdc2b70e9f77f47fdbe448aecadc
Clbotv111 <127049251+Clbotv111@users.noreply.github.com>
2023-03-06 01:34:05 +0530

    Update password.py

--- plugins/ExraMods/password.py
+++ plugins/ExraMods/password.py
@@ -15,5 +15,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('ᴅᴍx Bᴏᴛᴢ™️', url='https://t.me/dmx_bots')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('ᴄʟ ɢʀᴏᴜᴩ™️', url='https://t.me/+xRGPJsC0jAA1NTA1')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
